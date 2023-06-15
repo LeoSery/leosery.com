bc05bfc5f52cda6bf5e19f88e7e36e52e3bd6771
Séry Léo <seryleo@orange.fr>
2023-06-15 14:55:17 +0200

    Add: Tags for google analytics script. Update: CV.pdf file.

--- components/PDF/PDFviewer.jsx
+++ components/PDF/PDFviewer.jsx
@@ -1,32 +1,17 @@
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import { toolbarPlugin } from "@react-pdf-viewer/toolbar";
 import { ThemeContext } from "../../context/themeContext";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import React, { useContext } from "react";
 
 export default function PDFviewer() {
-  // const toolbarPluginInstance = toolbarPlugin({
-  //   searchPlugin: {
-  //     keyword: "PDF",
-  //   },
-  //   selectionModePlugin: {
-  //     selectionMode: SelectionMode.Hand,
-  //   },
-  // });
-
-  // const { Toolbar } = toolbarPluginInstance;
-
   const { isDark } = useContext(ThemeContext);
 
   return (
     <>
-      {/* <Toolbar /> */}
-      {/* <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.14.305/pdf.worker.min.js"> */}
-      <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js">
+      <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.14.305/pdf.worker.min.js">
         <Viewer
           fileUrl="/CV.pdf"
-          // plugins={[toolbarPluginInstance]}
           defaultScale={1}
           theme={isDark ? "light" : "dark"}
         />
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from "next-themes";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/global.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -14,6 +15,22 @@ export default function App({ Component, pageProps }) {
           <Footer />
         </ThemeProvider>
       </DarkThemeProvider>
+
+      <Script
+        id="googletagmanager"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NXQP4L74YT"
+      />
+      <Script id="analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NXQP4L74YT', {
+          page_path: window.location.pathname,
+          });
+      `}
+      </Script>
     </>
   );
 }

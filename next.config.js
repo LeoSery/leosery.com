8f93d4d4f4dc33be2d652f3dafd396b23f14c671
LeoSery <74504632+LeoSery@users.noreply.github.com>
2025-05-23 20:12:13 +0200

    Fix: Header of discord domain verification file.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  async headers() {
+    return [
+      {
+        source: '/.well-known/discord',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain; charset=utf-8',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
